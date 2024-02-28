c13e4863031a41067394244ee612e1e355dc33ad
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-02-28 11:50:05 -0800

    Initial Commit

---
+++ UnitTestCases/UnitTestCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestCasesApp.swift
+//  UnitTestCases
+//
+//  Created by ReetDhillon on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
